2393787e8c8916a3f319945a9d7314be92188e3b
marinasantangelo <marina.santangelo.n@gmail.com>
2019-09-11 17:48:53 -0300

    shows errors

--- main.js
+++ main.js
@@ -10,31 +10,39 @@ const app = new Vue({
     },
     methods:{
         calculo: function(){
-            return this.totalDesc = (this.totalEnvio*this.com)+this.totalEnvio;
+            this.totalDesc = (this.totalEnvio*this.com)+this.totalEnvio;
+            console.log('calculo');
+           
+            return this.totalDesc;
             
         },
 
         actualizaSaldo: function(){
+          console.log('as');
             this.saldo -= this.calculo(); 
             this.totalEnvio = 0;
+            console.log('saldo');
             return this.saldo;
         },
 
         alerta: function(){
             alert("Se han enviado " + this.totalEnvio + " BTC a " + this.address)
+            console.log('alerta');
         },
 
-        envio: function(){
-            this.alerta();
-            this.actualizaSaldo();
-           
-        }
-     /*checkForm: function (e) {
-        if (this.address && this.totalEnvio>0) {
-          return true;
-        }
+     checkForm: function (e) {
+       console.log(this.saldo);
+      e.preventDefault();
+       console.log(this.address)
+       this.errors = [];
+        if (this.address && this.totalEnvio>0 && this.totalDesc < this.saldo) {
+          this.alerta();
+          this.actualizaSaldo();
+
+         
+        } else{
   
-        this.errors = [];
+         
   
         if (!this.address) {
           this.errors.push('Por favor ingresar el BTC address de destino .');
@@ -43,11 +51,15 @@ const app = new Vue({
           this.errors.push('Por favor ingresar en monto BTC a enviar.');
         }
         if (this.totalDesc > this.saldo) {
-            this.errors.push('El total a descontar debe ser menor o igual a.'+ saldo);
+          console.log(this.saldo)
+            this.errors.push('El total a descontar debe ser menor o igual a.'+ this.saldo);
           }
-        e.preventDefault();
+        
+
       }
-    */
+      
+      }
+    
     
     
     }
