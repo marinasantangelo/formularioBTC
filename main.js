d9c33df181d42b9540e54f7f94b79da8e98f458a
marinasantangelo <marina.santangelo.n@gmail.com>
2019-09-11 21:32:55 -0300

    shows errors

--- main.js
+++ main.js
@@ -11,29 +11,28 @@ const app = new Vue({
     methods:{
         calculo: function(){
             this.totalDesc = (this.totalEnvio*this.com)+this.totalEnvio;
-            console.log('calculo');
-           
+                       
             return this.totalDesc;
             
         },
 
         actualizaSaldo: function(){
-          console.log('as');
+          
             this.saldo -= this.calculo(); 
             this.totalEnvio = 0;
-            console.log('saldo');
+         
             return this.saldo;
         },
 
         alerta: function(){
             alert("Se han enviado " + this.totalEnvio + " BTC a " + this.address)
-            console.log('alerta');
+            
         },
 
      checkForm: function (e) {
-       console.log(this.saldo);
+      
       e.preventDefault();
-       console.log(this.address)
+       
        this.errors = [];
         if (this.address && this.totalEnvio>0 && this.totalDesc < this.saldo) {
           this.alerta();
@@ -45,14 +44,13 @@ const app = new Vue({
          
   
         if (!this.address) {
-          this.errors.push('Por favor ingresar el BTC address de destino .');
+          this.errors.push('Por favor ingresar el BTC address de destino.');
         }
         if (this.totalEnvio<=0) {
           this.errors.push('Por favor ingresar en monto BTC a enviar.');
         }
         if (this.totalDesc > this.saldo) {
-          console.log(this.saldo)
-            this.errors.push('El total a descontar debe ser menor o igual a.'+ this.saldo);
+          this.errors.push('El total a descontar debe ser menor o igual a '+ this.saldo);
           }
         
 
